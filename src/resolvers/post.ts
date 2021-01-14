7edfaa5ab3b6e7c382bb9aeeeb0eec1b2c558638
kyawthura-gg <ggaryal@gmail.com>
2021-01-14 19:55:55 +0630

    add: edit show post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -8,4 +8,9 @@ export class PostResolver {
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
   }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
 }
